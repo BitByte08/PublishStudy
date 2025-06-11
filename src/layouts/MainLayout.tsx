e7aa92e2cbe4e856570a847c1327fba26b8407fb
bitbyte08 <me@bitworkspace.kr>
2025-06-12 01:44:51 +0900

    feat :: MainLayout 구현

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,27 @@
+import Sidebar from "@/components/layout/Sidebar.tsx";
+import styled from "@emotion/styled";
 
+const MainContainer = styled.div`
+    height: 100%;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 17.5rem 1fr;
+    grid-template-areas: "sidebar content";
+`;
 
-const MainLayout = ({children}: {children?: React.ReactNode}) => {
+const ContentContainer = styled.main`
+    grid-area: content;
+    overflow-y: auto;
+`;
+
+const MainLayout = ({children}:{children?:React.ReactNode}) => {
     return (
-        <>
-            Layout
-            {children}
-        </>
+        <MainContainer>
+            <Sidebar />
+            <ContentContainer>
+                {children}
+            </ContentContainer>
+        </MainContainer>
     )
 }
 
