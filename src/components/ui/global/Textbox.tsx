cb974195a35169e690afe1bf67e48abb85423332
bitbyte08 <me@bitworkspace.kr>
2025-06-16 00:42:39 +0900

    feat :: 글자 관련 컴포넌트 생성

---
+++ src/components/ui/global/Textbox.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const Title = styled.p`
+    margin: 0;
+    padding: 0;
+    font-size: 1.5rem;
+    font-weight: bold;
+`;
+export const Description = styled.p`
+    margin: 0;
+    padding: 0;
+    font-size: 1.125rem;
+`;
