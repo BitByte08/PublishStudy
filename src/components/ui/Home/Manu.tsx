e9538cda23ee01911c33865002def9f8a89f2f03
bitbyte08 <me@bitworkspace.kr>
2025-06-16 00:44:41 +0900

    feat :: 메뉴 컴포넌트 구현

--- src/components/ui/Home/Manu.tsx
+++ src/components/ui/Home/Manu.tsx
@@ -1,10 +1,48 @@
+import styled from "@emotion/styled";
+import {Title, Description} from "@/components/ui/global/Textbox";
 
-
+const MainContainer = styled.article`
+    display: grid;
+    grid-template-rows: 4.5rem 16.25rem;
+    gap: 1rem;
+`;
+const ButtonListContainer = styled.nav`
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
+    gap: 1.5rem;
+`;
+const ButtonContainer = styled.div`
+    background-color: #FAFAFA;
+    border-radius: .5rem;
+    padding: 1.5rem
+`;
 const Manu = () => {
     return (
-        <>
-            Manu
-        </>
+        <MainContainer>
+            <div>
+                <Title>메뉴</Title>
+                <Description>원하는 항목을 선택하여 이동하세요.</Description>
+            </div>
+            <ButtonListContainer>
+                <ButtonContainer>
+                    <Title>역대 전공동아리 보러가기</Title>
+                    <Description>바로가기</Description>
+                </ButtonContainer>
+                <ButtonContainer>
+                    <Title>역대 전공동아리 보러가기</Title>
+                    <Description>바로가기</Description>
+                </ButtonContainer>
+                <ButtonContainer>
+                    <Title>역대 전공동아리 보러가기</Title>
+                    <Description>바로가기</Description>
+                </ButtonContainer>
+                <ButtonContainer>
+                    <Title>역대 전공동아리 보러가기</Title>
+                    <Description>바로가기</Description>
+                </ButtonContainer>
+            </ButtonListContainer>
+        </MainContainer>
     )
 }
 
